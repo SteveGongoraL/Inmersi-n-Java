6b4746923b82862c25d0b196178c0eb392629082
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-05-06 00:18:34 -0600

    Agregando unos comentarios en Main

--- Main.java
+++ Main.java
@@ -3,33 +3,36 @@ import modelo.Serie;
 
 public class Main{
     public static void main(String[] args) {
+        // Utilizando el menu para guardar datos dinamicos
         Principal principal = new Principal();
         principal.muestraElMenu();
 
-        // // Guardar una pelicula
+        // Guardando datos de forma manual
+
+        // Guardar una pelicula
         // Pelicula interestelar = new Pelicula();
         // interestelar.setNombre("Interestelar");
         // interestelar.setTiempoDeDuracionEnMinutos(139);
         // interestelar.setFechaDeLanzamiento(2014);
-        // // Imprimir los valores por medio del Metodo
+        // Imprimir los valores por medio del Metodo
         // interestelar.muestraFichaTecnica();
 
-        // // Guardar otra pelicula
+        // Guardar otra pelicula
         // Pelicula avengers = new Pelicula();
         // avengers.setNombre("Avengers");
         // avengers.setTiempoDeDuracionEnMinutos(143);
         // avengers.setFechaDeLanzamiento(2012);
-        // // Imprimir los valores por medio del Metodo
+        // Imprimir los valores por medio del Metodo
         // avengers.muestraFichaTecnica();
 
-        // // Guardar una serie
+        // Guardar una serie
         // Serie friends = new Serie();
         // friends.setNombre("Friends");
         // friends.setFechaDeLanzamiento(1994);
         // friends.setTemporadas(10);
         // friends.setEpisodiosPorTemporada(22);
         // friends.setDuracionEnMinutosPorEpisodio(30);
-        // // Imprimir los valores por medio del Metodo
+        // Imprimir los valores por medio del Metodo
         // friends.muestraFichaTecnica();
     }
 }
